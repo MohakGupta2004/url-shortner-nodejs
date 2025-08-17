6db664c347d355760f523c500d1cd2793b2c04ac
mohak <mohakgupta500@gmail.com>
2025-08-17 23:12:09 +0530

    bcryptjs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,27 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
-
+import bcrypt from "bcrypt"
 const registerController = async (req, res) => {
     const {username, email, password} = req.body; 
+    const existingUser = await User.findOne({
+        username
+    })
+    if(existingUser){
+        res.json({
+            message: "User already exist"
+        })
+        return;
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
         username,
         email,
-        password        
+        password: hashPassword        
+    })
+
+    res.json({
+        message: "User created",
+        user
     })
     
 }
@@ -14,7 +29,6 @@ const loginController = async (req, res)=>{
     const {email, password} = req.body; 
     const user = await User.findOne({
         email,
-        password        
     })
     if(!user){
         res.json({
@@ -22,6 +36,15 @@ const loginController = async (req, res)=>{
         })
         return;
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+        res.json({
+            message: "Password not valid"
+        })
+        return;
+    }
+
     const token = await jwt.sign({
         "username": user.username
     }, process.env.JWT_SECRET, {
